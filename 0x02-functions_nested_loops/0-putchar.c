10b106e37339ac2b178224c078f14a20309154cb
Nurudeen Oyelowo <neroevali4u@yahoo.co.uk>
2022-12-13 07:51:19 -0800

    Functions and Nested Loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,17 +4,16 @@
  * main - A program that prints _putchar
  * Return: 0
  */
-
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	char text[9] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+
+	return (0);
 }
