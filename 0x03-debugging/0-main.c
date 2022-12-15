7dc7cb4749ee4ac4f28a18801c78754daf09406e
Nurudeen Oyelowo <neroevali4u@yahoo.co.uk>
2022-12-15 11:55:17 -0800

    Functions and Nested Loops

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 void positive_or_negative(int i);
@@ -12,7 +11,7 @@ int main(void)
 {
 	int i;
 
-	i = 0;
+	i = 98;
 	positive_or_negative(i);
 
 	return (0);
